8cb22056b2ab8473c3d9ffceb5d77106cd7eca28
Aquiles Aguiar <67012626+AquilesAguiar@users.noreply.github.com>
2022-06-27 16:03:28 -0300

    Adicionando cors

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request
 from users import *
 from recomendacao import getSimilares
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def index():
